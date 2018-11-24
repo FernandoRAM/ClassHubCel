2668b8fcc0691fc5bd48a5c598c8c8b7e4887e64
rambyBoy <rmendoza29@alumnos.uaq.mx>
2018-11-24 16:20:56 -0600

    Ya al fin

--- img_compress/index.php
+++ img_compress/index.php
@@ -18,8 +18,26 @@
 	<?php
 
 	function resizeImage( $resourceType, $image_width, $image_height ) {
-		$resizeWidth = 100;
-		$resizeHeight = 100;
+		if ( $image_width > $image_height ) { // Proporción en base al ancho
+			if ( $image_width > 1920 ) { // El ancho máximo será de 1920
+				$resizeWidth = 1920; // Nuevo valor del ancho
+				$p = (( $resizeWidth * 100 / $image_width ) * 0.01); // Porcentaje de reducción
+				$resizeHeight = (integer) ($p * $image_height); // Nuevo valor de la altura en base al nuevo ancho
+			} else {
+				$resizeWidth = $image_width; // La imagen conserva su ancho original
+				$resizeHeight = $image_height; // La imagen conserva su altura original
+			}
+		} else { // Proporción en base a la altura
+			if ( $image_height > 1920 ) { // La altura máxima será de 1920
+				$resizeHeight = 1920; // Nuevo valor de la altura
+				$p = (( $resizeHeight * 100 / $image_height ) * 0.01); // Porcentaje de reducción
+				$resizeWidth = (integer) ($p * $image_height); // Nuevo valor de la altura en base a la nueva altura
+			} else {
+				$resizeWidth = $image_width; // La imagen conserva su ancho original
+				$resizeHeight = $image_height; // La imagen conserva su altura original
+			}
+		}
+		echo "<script>alert('X: ' + $resizeWidth + ' Y: ' + $resizeHeight)</script>";
 		$imageLayer = imagecreatetruecolor( $resizeWidth, $resizeHeight );
 		imagecopyresampled( $imageLayer, $resourceType, 0, 0, 0, 0, $resizeWidth, $resizeHeight, $image_width, $image_height );
 		return $imageLayer;
@@ -33,9 +51,9 @@
 			$resizeFileName = time();
 			$uploadPath = "./uploads/";
 			$fileExt = pathinfo( $_FILES[ 'upload_image' ][ 'name' ], PATHINFO_EXTENSION );
-			$uploadImageType = $sourceProperties[ 2 ];
 			$sourceImageWidth = $sourceProperties[ 0 ];
 			$sourceImageHeight = $sourceProperties[ 1 ];
+			$uploadImageType = $sourceProperties[ 2 ];
 			switch ( $uploadImageType ) {
 				case IMAGETYPE_JPEG:
 					$resourceType = imagecreatefromjpeg( $fileName );
