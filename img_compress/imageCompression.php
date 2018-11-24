d4cc7c040bc57561322315ba9b589a0af4f87aef
rambyBoy <rmendoza29@alumnos.uaq.mx>
2018-11-24 14:37:01 -0600

    3

--- img_compress/imageCompression.php
+++ img_compress/imageCompression.php
@@ -11,65 +11,65 @@
 		<input type="submit" name="form_submit" value="Submit">
 	</form>
 
+<?php
+function resizeImage($resourceType,$image_width,$image_height) {
+    $resizeWidth = 100;
+    $resizeHeight = 100;
+    $imageLayer = imagecreatetruecolor($resizeWidth,$resizeHeight);
+    imagecopyresampled($imageLayer,$resourceType,0,0,0,0,$resizeWidth,$resizeHeight, $image_width,$image_height);
+    return $imageLayer;
+}
+ 
+if(isset($_POST["form_submit"])) {
+	$imageProcess = 0;
+    if(is_array($_FILES)) {
+        $fileName = $_FILES['upload_image']['tmp_name']; 
+        $sourceProperties = getimagesize($fileName);
+        $resizeFileName = time();
+        $uploadPath = "./uploads/";
+        $fileExt = pathinfo($_FILES['upload_image']['name'], PATHINFO_EXTENSION);
+        $uploadImageType = $sourceProperties[2];
+        $sourceImageWidth = $sourceProperties[0];
+        $sourceImageHeight = $sourceProperties[1];
+        switch ($uploadImageType) {
+            case IMAGETYPE_JPEG:
+                $resourceType = imagecreatefromjpeg($fileName); 
+                $imageLayer = resizeImage($resourceType,$sourceImageWidth,$sourceImageHeight);
+                imagejpeg($imageLayer,$uploadPath."thump_".$resizeFileName.'.'. $fileExt);
+                break;
+ 
+            case IMAGETYPE_GIF:
+                $resourceType = imagecreatefromgif($fileName); 
+                $imageLayer = resizeImage($resourceType,$sourceImageWidth,$sourceImageHeight);
+                imagegif($imageLayer,$uploadPath."thump_".$resizeFileName.'.'. $fileExt);
+                break;
+ 
+            case IMAGETYPE_PNG:
+                $resourceType = imagecreatefrompng($fileName); 
+                $imageLayer = resizeImage($resourceType,$sourceImageWidth,$sourceImageHeight);
+                imagepng($imageLayer,$uploadPath."thump_".$resizeFileName.'.'. $fileExt);
+                break;
+ 
+            default:
+                $imageProcess = 0;
+                break;
+        }
+        move_uploaded_file("e", $uploadPath. $resizeFileName. ".". $fileExt);
+        $imageProcess = 1;
+    }
+ 
+	if($imageProcess == 1){
+	?>
+		<b>Comprimida</b>
 	<?php
-
-	function resizeImage( $resourceType, $imageWidth, $imageHeight ) {
-		$resizeWidth = 250;
-		$resizeHeight = 250;
-		$imageLayer = imagecreatetruecolor( $resizeWidth, $resizeHeight );
-		imagecopyresampled( $imageLayer, $resourceType, 0, 0, 0, 0, $resizeWidth, $resizeHeight, $imageWidth, $imageHeight );
-		return $imageLayer;
+	}else{
+	?>
+		<b>No comprimida</b>
+	<?php
 	}
-
-	if ( isset( $_POST[ 'form_submit' ] ) ) {
-		$imageProcess = 0;
-		if ( is_array( $_FILES ) ) {
-			$fileName = $_FILES[ 'upload_image' ][ 'tmp_name' ];
-			$sourceProperties = getimagesize( $fileName );
-			$resizeFileName = time();
-			$uploadPath = './uploads/';
-			$fileExt = pathinfo( $_FILES[ 'upload_image' ][ 'name' ], PATHINFO_EXTENSION );
-			$sourceImageWidth = $sourceProperties[ 0 ];
-			$sourceImageHeight = $sourceProperties[ 1 ];
-			$uploadImageType = $sourceProperties[ 2 ];
-
-			switch ( $uploadImageType ) {
-				case IMAGETYPE_JPEG:
-					$resourceType = imagecreatefromjpeg( $fileName );
-					$imageLayer = resizeImage( $resourceType, $resourceImageWidth, $resourceImageHeight );
-					imagejpeg( $imageLayer, $uploadPath . "thump_" . $resizeFileName . "." . $fileExt );
-					break;
-
-				case IMAGETYPE_GIF:
-					$resourceType = imagecreatefromgif( $fileName );
-					$imageLayer = resizeImage( $resourceType, $resourceImageWidth, $resourceImageHeight );
-					imagegif( $imageLayer, $uploadPath . "thump_" . $resizeFileName . "." . $fileExt );
-					break;
-
-				case IMAGETYPE_PNG:
-					$resourceType = imagecreatefrompng( $fileName );
-					$imageLayer = resizeImage( $resourceType, $resourceImageWidth, $resourceImageHeight );
-					imagepng( $imageLayer, $uploadPath . "thump_" . $resizeFileName . "." . $fileExt );
-					break;
-					
-				default:
-					$imageProcess = 0;
-					break;
-			}
-			
-			move_uploaded_file($file, $uploadPath . $resizeFileName . "." . $fileExt);
-			$imageProcess = 1;
-		
-		}
-		
-		if($imageProcess == 1) {
-		?>
-	
-		<br><br>
-		<b>Imagen comprimida con éxito</b>
-		}
-		
-	}
+	$imageProcess = 0;
+}
+?>
 	
 </body>
 </html>
