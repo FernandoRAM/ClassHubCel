3e32fd20a229f1fb714c662971299277c4556156
rambyBoy <rmendoza29@alumnos.uaq.mx>
2018-12-05 17:23:42 -0600

    Ya sube imágenes en la versión móvil

---
+++ subirImagen/www/js/upload.js
@@ -0,0 +1,15 @@
+var feedback = function(res) {
+    if (res.success === true) {
+        var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
+        document.querySelector('.status').classList.add('bg-success');
+        document.querySelector('.status').innerHTML =
+            'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+		document.write("<img src='"+get_link+"'>Link</img>");
+		return get_link;
+    }
+};
+
+new Imgur({
+    clientid: '8087135b7076f6b', //You can change this ClientID
+    callback: feedback
+});
